0643bfc4a97858af419f338614f7467758280535
TechCrafter07 <techcrafter07@gmail.com>
2021-03-01 18:22:36 +0100

    Final

--- api/dbconnection.php
+++ api/dbconnection.php
@@ -7,15 +7,17 @@
     $conn = mysqli_connect($host, $user, $pass, $database) or die($conn -> mysqli_error);
 
     function update($conn, $open, $high, $low, $close, $date, $id){
+        echo $date;
+        echo "<br>";
         $result = mysqli_query($conn, "SELECT * FROM apidata");
         if(mysqli_num_rows($result) > 25){
             mysqli_query($conn, "UPDATE apidata SET open = '{$open}', high = '{$high}', low = '{$low}', close = '{$close}', date = '{$date}' WHERE ID = '{$id}'");
-            mysqli_query($conn, "UPDATE konfig SET trash = 5 WHERE ID = 1 ");
-            
+            mysqli_query($conn, "UPDATE konfig SET trash = 6 WHERE id = 1 ");
+
         }
         else{
             mysqli_query($conn, "INSERT INTO apidata (open, high, low, close, date) VALUES ('{$open}', '{$high}', '{$low}', '{$close}' , '{$date}')");
-            mysqli_query($conn, "UPDATE konfig SET trash = 5 WHERE ID = 1 ");
+            mysqli_query($conn, "UPDATE konfig SET trash = 6 WHERE id = 1 ");
         }
         
     }
